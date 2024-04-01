67f1122fbeda02db4e16950bdc04c543b1541dd6
zwithe <zwithe133@gmail.com>
2024-04-01 14:33:25 -0400

    part 4 done

--- functions.py
+++ functions.py
@@ -88,6 +88,65 @@ def recursive_reverse(string, i=0):
     return string[-1-i] + recursive_reverse(string, i+1)
 
 
+def max_num(a,b,c):
+  return max([a,b,c])
+
+
+def mult_list(list):
+  if len(list) == 0:
+    return 0
+
+  product = list[0]
+
+  if len(list) > 1:
+    for i in list[1:]:
+      product = product * i
+
+  return product
+    
+
+def rev_string(string):
+  return string[::-1]
+
+
+def num_within(num,min,max):
+  return num in range(min,max+1)
+     
+
+triangle = [[1],[1,1]]
+def pascal(n):
+  #base case
+  if n < 1:
+    print("invalid number of rows")
+  elif n == 1:
+    print(triangle[0])
+  else:
+    row_number = 2
+    #fill up correct number of rows in triangle
+    while len(triangle) < n:
+      row = []
+      row_prev = triangle[row_number - 1]
+      #create correct row, then add to triangle (this row will be 1 longer than row before it)
+      length = len(row_prev)+1
+      for i in range(length):
+        #first number is 1
+        if i == 0:
+          row.append(1)
+        #intermediate nunmbers get added from previous rows
+        elif i > 0 and i < length-1:
+          row.append(triangle[row_number-1][i-1]+triangle[row_number-1][i])
+        #last number is 1
+        else:
+          row.append(1)
+      triangle.append(row)
+      row_number += 1
+
+    #print triangle
+    for row in triangle:
+      print(row)
+
+pascal(2)
+pascal(5)
 
 hello()
 
@@ -135,4 +194,19 @@ print(recursive_reverse("aaaa"))
 print(recursive_reverse("aaba"))
 print(recursive_reverse("apple"))
 print(recursive_reverse(""))
+
+print(max_num(1,2,3))
+print(max_num(100,50,1))
+print(max_num(15,30,2))
+
+print(mult_list([1,2,3]))
+print(mult_list([]))
+print(mult_list([15]))
+
+print(rev_string(""))
+print(rev_string("apple"))
+
+print(num_within(3,2,4))     
+print(num_within(3,1,3))     
+print(num_within(10,2,5))
 
